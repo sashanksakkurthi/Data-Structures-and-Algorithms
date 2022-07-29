b231f9553d2bf35cac888b6b2a16a90c97a55a60
Sakkurthi Sashank <sakkurthisashank@gmail.com>
2022-07-29 16:45:41 +0530

    python to c++

---
+++ problems/fibonacci_number.cpp
@@ -0,0 +1,30 @@
+// The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+// F(0) = 0, F(1) = 1
+// F(n) = F(n - 1) + F(n - 2), for n > 1.
+// Given n, calculate F(n).
+
+// Example 1:
+// Input: n = 2
+// Output: 1
+// Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+
+// Example 2:
+// Input: n = 3
+// Output: 2
+// Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+
+#include <iostream>
+
+using namespace std;
+
+int solution(int n){
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    return solution(n-1) + solution(n-2);
+}
+
+int main(){
+    cout << solution(5);
+}
+
